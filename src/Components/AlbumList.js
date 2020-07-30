03c9105905255aba9d3d4c30f380fd06848facad
nguyenxuankien2019 <nguyenxuankien1997@gmail.com>
2020-07-30 16:47:01 +0700

    289 - Class Based Component

--- src/Components/AlbumList.js
+++ src/Components/AlbumList.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Text, StyleSheet, View } from 'react-native';
 
-export default function AlbumList() {
+export default class AlbumList extends Component {
+    render(){
     return (
         <View >
              <Text style={{textAlign:'center'}}>Album List!!</Text>
         </View>
     )
+}        
 }
 const styles = StyleSheet.create({
 
